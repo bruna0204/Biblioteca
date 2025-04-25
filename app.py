4d7567c81b1fe4ec51093a50f9dfc5588c7efa46
bruna0204 <bruna02042008@gmail.com>
2025-04-25 16:19:49 -0300

    25/04

---
+++ app.py
@@ -0,0 +1,355 @@
+from flask_pydantic_spec import FlaskPydanticSpec
+import sqlalchemy
+from flask import Flask, render_template, request, redirect, url_for, flash,jsonify
+from sqlalchemy import select, func, desc
+from sqlalchemy.exc import SQLAlchemyError
+
+from models import Usuario, Livro, Emprestimo, db_session
+from sqlalchemy import func
+
+app = Flask(__name__)
+spec = FlaskPydanticSpec('flask',
+                          title='First API - SENAI',
+                          version='1.0.0',)
+spec.register(app)
+
+ # corrigir
+
+@app.route('/cadastro_usuario', methods=["POST"])
+def criar_usuario():
+    """
+    API para cadastrar o usuario
+
+    ### Resposta (JSON):
+    '''json{
+        "status": "sucesso",
+        "mensagem":"Usuario criado com sucesso",
+        "id": post.id_usuario,
+        "nome": informacoes["nome"],
+        "cpf": informacoes["CPF"],
+        "endereco": informacoes["endereco"]
+        }
+
+    :return: cadastra o usuario
+    """
+    try:
+        informacoes = request.get_json()
+        print(informacoes)
+        if not "nome" in informacoes or not "CPF" in informacoes or not "endereco" in informacoes:
+            return jsonify({'status': 'erro','mensagem': 'Obrigatorio Nome, CPF e endereco'})
+
+        if informacoes["endereco"] == "" or informacoes["nome"] == "" or informacoes["CPF"] == "":
+            return jsonify({'status': 'erro', 'mensagem': 'Erro preencha os espaços'})
+        #if existe_cpf:
+         #   return jsonify({'status': "erro", 'mensagem': 'usuario ja existente'})
+
+        post = Usuario(Nome = informacoes["nome"],
+                        CPF = informacoes["CPF"],
+                        endereco = informacoes["endereco"]
+
+                        )
+        post.save()
+
+        return jsonify(
+            {
+                "status": "sucesso",
+                "mensagem":"Usuario criado com sucesso",
+                "id": post.id_usuario,
+                "nome": informacoes["nome"],
+                "cpf": informacoes["CPF"],
+                "endereco": informacoes["endereco"]
+            })
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+@app.route('/lista_usuario', methods=["GET"])
+def listar_usuario():
+    """
+        API para listar o usuario
+
+        ### Resposta (JSON):
+        '''json
+        {'usuarios': lista_usuario}
+
+
+        :return: a lista ou a mensagem de erro
+    """
+    try:
+        sql_usuario = select(Usuario)
+        result = db_session.execute(sql_usuario).scalars()
+        lista_usuario = []
+        for usuario in result:
+            lista_usuario.append(usuario.serialize_usuarios())
+        return jsonify({'usuarios': lista_usuario})
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+
+@app.route('/atualizar_usuario/<int:id_usuario>', methods=["PUT"])
+def atualizar_usuario(id_usuario):
+    """
+    API para atualizar o livro
+    ### Resposta (JSON):
+    jsonify({'status': 'sucesso'})
+
+    :param id_usuario: id do usuario que vai ser atualizado para assim localizado
+    :return: atualiza o usuario
+    """
+    try:
+        usuario = db_session.execute(select(Usuario).where(Usuario.id_usuario == id_usuario)).scalar()
+        if usuario is None:
+            return jsonify({"status": "erro", "mensagem": "usuario não encontrado"})
+
+        dados_usuario = request.get_json()
+
+        usuario.Nome = dados_usuario["nome"]
+        usuario.CPF = dados_usuario["CPF"]
+        usuario.endereco = dados_usuario["endereco"]
+        usuario.save()
+
+        return jsonify({'status': 'sucesso'})
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+
+
+
+@app.route('/cadastrar_emprestimo', methods=["POST"])
+def criar_emprestimo():
+    """
+        API para cadastrar o emprestimo
+        verifica se os campos estão vazios
+        verifia se existe o id do usuario e do livro
+        e verifica se o livro ja esta emprestado
+
+        ### Resposta (JSON):
+        '''json{
+            "status": "sucesso",
+            "mensagem":"Emprestimo criado com sucesso",
+            "data_de_emprestimo": informacoes["data_de_emprestimo"],
+            "data_de_devolucao": informacoes["data_de_devolucao"],
+            "id_livro": informacoes["id_livro"],
+            "id_usuario": informacoes["id_usuario"]
+            }
+
+        :return: o cadastro do emprestimo dos livros
+        """
+
+    try:
+        informacoes = request.get_json()
+
+        print("dados :",informacoes)
+        livro = db_session.execute(select(Livro).where(Livro.id_livro == informacoes["id_livro"])).scalar()
+        usuario = db_session.execute(select(Usuario).where(Usuario.id_usuario == informacoes["id_usuario"])).scalar()
+        print("usuarios :",usuario)
+        print("livro :",livro)
+
+        if not livro:
+            print("controle")
+            if not usuario:
+                return jsonify({"status": "erro", "mensagem": "usuario nem livro encontrado"})
+            else:
+                return jsonify({"status": "erro", "mensagem": "livro não encontrado"})
+        if not usuario:
+            return jsonify({"status": "erro", "mensagem": "usuario não encontrado"})
+
+        emprestimo_existente = db_session.execute(
+            select(Emprestimo).where(
+                (Emprestimo.id_livro == informacoes["id_livro"]) &
+                (Emprestimo.id_usuario == informacoes["id_usuario"])
+            )
+        ).scalar()
+
+        if emprestimo_existente:
+            return jsonify({"status": "erro", "mensagem": "emprestimo existente"})
+
+
+
+        if not "data_de_emprestimo" in informacoes or not "data_de_devolucao" or not "id_livro" in informacoes or not "id_usuario" in informacoes:
+            return jsonify({'status': 'erro','mensagem': 'Obrigatorio Nome, CPF e endereco'})
+
+        if informacoes["data_de_emprestimo"] == "" or informacoes["data_de_devolucao"] == "" or informacoes["id_livro"] == "" or informacoes["id_usuario"] == "":
+            return jsonify({'status': 'erro', 'mensagem': 'Erro preencha os espaços'})
+
+
+        post = Emprestimo(data_de_emprestimo = informacoes["data_de_emprestimo"],
+                        data_de_devolucao = informacoes["data_de_devolucao"],
+                        id_livro = informacoes["id_livro"],
+                        id_usuario= informacoes["id_usuario"]
+                        )
+        post.save()
+
+
+
+        return jsonify(
+            {
+                "status": "sucesso",
+                "mensagem":"Emprestimo criado com sucesso",
+                "data_de_emprestimo": informacoes["data_de_emprestimo"],
+                "data_de_devolucao": informacoes["data_de_devolucao"],
+                "id_livro": informacoes["id_livro"],
+                "id_usuario": informacoes["id_usuario"]
+            }
+        )
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+
+@app.route('/lista_emprestimo', methods=["GET"])
+def listar_emprestimo():
+    """
+           API para listar o emprestimo
+
+           ### Resposta (JSON):
+           '''json
+           {'emprestimo': lista_emprestimo}
+
+
+           :return: a lista ou a mensagem de erro
+    """
+
+    try:
+        sql_emprestimo = select(Emprestimo)
+        result = db_session.execute(sql_emprestimo).scalars()
+        lista_emprestimo = []
+        for emprestimo in result:
+            lista_emprestimo.append(emprestimo.serialize_emprestimo())
+        return jsonify({'emprestimo': lista_emprestimo})
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+
+@app.route('/atualizar_emprestimo/<int:id_emprestimo>', methods=["PUT"])
+def atualizar_emprestimo(id_emprestimo):
+    """
+    API para atualizar o livro
+    ### Resposta (JSON):
+    jsonify({'status': 'sucesso'})
+
+    :param id_emprestimo: id do emprestimo que vai ser atualizado para assim localizado
+    :return: atualiza o emprestimo
+    """
+    try:
+        emprestimo = db_session.execute(select(Emprestimo).where(Emprestimo.id_emprestimo == id_emprestimo)).scalar()
+        if emprestimo is None:
+            return jsonify({"status": "erro", "mensagem": "emprestimo não encontrado"})
+
+        dados_emprestimo = request.get_json()
+
+        emprestimo.data_de_emprestimo = dados_emprestimo["data_de_emprestimo"]
+        emprestimo.data_de_devolucao = dados_emprestimo["data_de_devolucao"]
+        emprestimo.id_livro = dados_emprestimo["id_livro"]
+        emprestimo.id_usuario = dados_emprestimo["id_usuario"]
+
+        emprestimo.save()
+
+        return jsonify({'status': 'sucesso'})
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+
+
+@app.route('/cadastro_livro', methods=["POST"])
+def criar_livro():
+    """
+           API para cadastrar o livro
+
+           ### Resposta (JSON):
+           '''json{
+               "status": "sucesso",
+                "mensagem":"livro- criado com sucesso",
+                "id": post.id_livro,
+                "titulo": informacoes["titulo"],
+                "autor": informacoes["autor"],
+                "ISBN": informacoes["ISBN"],
+                "resumo": informacoes["resumo"]
+               }
+
+           :return: o cadastro do livro
+           """
+    try:
+        informacoes = request.get_json()
+        # relacao = db_session.execute(select(Livro).where(Livro.id_livro == informacoes["id_livro"])).scalar()
+        print(informacoes)
+        if not "titulo" in informacoes or not "autor" in informacoes or not "ISBN" in informacoes or not "resumo" in informacoes:
+            return jsonify({'status': 'erro','mensagem': 'Obrigatorio Nome, CPF e endereco'})
+
+        if informacoes["titulo"] == "" or informacoes["autor"] == "" or informacoes["ISBN"] == "" or informacoes["resumo"] == "":
+            return jsonify({'status': 'erro', 'mensagem': 'Erro preencha os espaços'})
+
+        post = Livro(titulo = informacoes["titulo"],
+                        autor = informacoes["autor"],
+                        ISBN = informacoes["ISBN"],
+                        resumo = informacoes["resumo"]
+                        )
+        post.save()
+
+        return jsonify(
+            {
+                "status": "sucesso",
+                "mensagem":"livro- criado com sucesso",
+                "id": post.id_livro,
+                "titulo": informacoes["titulo"],
+                "autor": informacoes["autor"],
+                "ISBN": informacoes["ISBN"],
+                "resumo": informacoes["resumo"]
+            }
+        )
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+
+@app.route('/lista_livro', methods=["GET"])
+def listar_livros():
+    """
+           API para listar os livros
+
+           ### Resposta (JSON):
+           '''json
+           {'livros': lista_livros}
+
+           :return: a lista ou a mensagem de erro
+    """
+    try:
+        sql_livro = select(Livro)
+        result = db_session.execute(sql_livro).scalars()
+        lista_livro = []
+        for livro in result:
+            lista_livro.append(livro.serialize_livro())
+        return jsonify({'livros': lista_livro})
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+@app.route('/atualizar_livro/<int:id_livro>', methods=["PUT"])
+def atualizar_livro(id_livro):
+    """
+    API para atualizar o livro
+    ### Resposta (JSON):
+    jsonify({'status': 'sucesso'})
+
+    :param id_livro: id do livro que vai ser atualizado para assim localizado
+    :return: atualiza o livro
+    """
+    try:
+        livro = db_session.execute(select(Livro).where(Livro.id_livro == id_livro)).scalar()
+        if livro is None:
+            return jsonify({"status": "erro", "mensagem": "livro não encontrado"})
+
+        dados_livro = request.get_json()
+
+        livro.titulo = dados_livro["titulo"]
+        livro.autor = dados_livro["autor"]
+        livro.ISBN = dados_livro["ISBN"]
+        livro.resumo = dados_livro["resumo"]
+        livro.save()
+
+        return jsonify({'status': 'sucesso'})
+    except SQLAlchemyError as e:
+        return jsonify({'status': 'erro', 'mensagem': str(e)})
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,125 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Float
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship, declarative_base
+#configuração base de dados
+engine = create_engine('sqlite:///controle_estoque.sqlite3') #nome do banco
+db_session = scoped_session(sessionmaker(bind=engine))
+
+
+#modo declarativo
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+#Pessoas que tem atividade
+class Usuario(Base):
+    __tablename__ = 'tab_usuarios'
+    id_usuario = Column(Integer, primary_key=True)
+    Nome = Column(String(30), nullable=False, index=True )
+    endereco = Column(String(70), nullable=False, index=True) #string o tamanho dele
+    CPF = Column(Integer, nullable=False, index=True, unique=True) #string o tamanho dele
+
+
+    def __repr__(self):
+        return '<Funcionario: {} {} {} {}>' .format(self.id_usuario, self.CPF, self.Nome, self.endereco, )
+
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+#função para deletar
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+
+    def serialize_usuarios(self):
+        dados_usuario = {
+            'id_usuarios': self.id_usuario,
+            'CPF': self.CPF,
+            'Nome': self.Nome,
+            'endereca': self.endereco,
+        }
+        return dados_usuario
+
+
+
+class Livro(Base):
+    __tablename__ = 'tab_livros'
+    id_livro = Column(Integer, primary_key=True)
+    titulo = Column(String(70), nullable=False, index=True)
+    autor = Column(String(70), nullable=False, index=True)
+    ISBN = Column(Integer, nullable=False, index=True)
+    resumo = Column(String(100), nullable=False, index=True)
+
+    def __repr__(self):
+        return '<Livros: {} {} {} {}>' .format(self.id_livro, self.titulo, self.ISBN, self.resumo, self.autor)
+
+#função para salvar no banco
+
+    def save(self):
+        db_session.add(self)#seção de acesso
+        db_session.commit() #salva a informação
+
+#função para deletar
+    def delete(self):
+        db_session.delete(self)#deletar
+        db_session.commit()# salvar
+
+    def serialize_livro(self):
+        dados_livros = {
+            'id_livro': self.id_livro,
+            'titulo': self.titulo,
+            'autor': self.autor,
+            'resumo': self.resumo,
+            'ISBN': self.ISBN,
+        }
+        return dados_livros
+
+
+
+
+
+
+class Emprestimo(Base):
+    __tablename__ = 'TAB_EMPRESTIMO' #nome da tabela
+    id_emprestimo = Column(Integer, primary_key=True) #chave primaria (unico) integer = tipo de dado
+    data_de_emprestimo = Column(String(20), nullable=False, index=True) #nullable tem que obrigatoriamente preencher o espaço
+    data_de_devolucao = Column(String(27), nullable=False, index=True)# index pesquisa
+    id_usuario = Column(Integer, ForeignKey('tab_usuarios.id_usuario'), nullable=False)#string o tamanho dele
+    usuario = relationship('Usuario')
+    id_livro = Column(Integer, ForeignKey('tab_livros.id_livro'), nullable=False)
+    produto = relationship('Livro')
+    #colum = coluna
+
+    def __repr__(self):
+        return '<Emprestimo: {} {} {} {} {}>' .format(self.id_emprestimo, self.data_de_emprestimo, self.data_de_devolucao, self.id_livro, self.id_usuario)#self ele chama ele mesmo
+
+#função para salvar no banco
+
+    def save(self):
+        db_session.add(self)#seção de acesso
+        db_session.commit() #salva a informação
+
+#função para deletar
+    def delete(self):
+        db_session.delete(self)#deletar
+        db_session.commit()# salvar
+
+    def serialize_emprestimo(self):
+        dados_emprestimo = {
+            'id_emprestimo': self.id_emprestimo,
+            'data_de_emprestimo': self.data_de_emprestimo,
+            'data_de_devolucao': self.data_de_devolucao,
+            'id_usuario': self.id_emprestimo,
+            'id_livro': self.id_livro,
+
+        }
+        return dados_emprestimo
+
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+if __name__ == '__main__':
+    init_db()
